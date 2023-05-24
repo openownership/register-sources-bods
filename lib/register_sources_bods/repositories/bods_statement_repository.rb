6b2658dd680b21b79b221301af7c834c7b2a4d61
Josh Williams <josh@spacesnottabs.com>
2023-05-24 19:04:13 +0000

    Fixed typo for getting bulk ids

--- lib/register_sources_bods/repositories/bods_statement_repository.rb
+++ lib/register_sources_bods/repositories/bods_statement_repository.rb
@@ -50,7 +50,7 @@ module RegisterSourcesBods
       end
 
       def get_bulk(statement_ids)
-        return [] unless statement_ids
+        return [] unless statement_ids && !statement_ids.empty?
 
         process_results(
           client.search(
