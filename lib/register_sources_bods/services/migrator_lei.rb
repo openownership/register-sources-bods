a31c6cd635350c45ee0de9438f05392afcda9468
Nicolas Williams <tp@tiredpixel.com>
2023-08-30 19:17:54 +0200

    [LEI-4] Services::MigratorLEI: add, with bin script
    This is a one-time script for looping through all documents contained
    within the `add_ids` Elasticsearch index, checking if they already have
    an LEI within their identifiers, and adding the LEI and republishing the
    BODS statement if not.
    
    https://github.com/openownership/register-sources-oc/issues/14

---
+++ lib/register_sources_bods/services/migrator_lei.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'register_sources_oc/repositories/add_id_repository'
+
+require_relative '../mappers/resolver_mappings'
+require_relative '../repositories/bods_statement_repository'
+require_relative '../services/publisher'
+
+module RegisterSourcesBods
+  module Services
+    class MigratorLEI
+      include RegisterSourcesBods::Mappers::ResolverMappings
+
+      def initialize(
+        add_id_repository: RegisterSourcesOc::Repositories::AddIdRepository.new,
+        publisher: Services::Publisher.new,
+        bods_statement_repository: Repositories::BodsStatementRepository.new
+      )
+        @publisher = publisher
+        @repository_ai = add_id_repository
+        @repository_bs = bods_statement_repository
+      end
+
+      def migrate(jurisdiction_codes: [], uids: [])
+        @repository_ai.each_lei(jurisdiction_codes:, uids:) do |add_id|
+          id_lei = identifier_lei_from_add_id(add_id)
+          id_oc = identifier_open_corporates_from_company(add_id.jurisdiction_code, add_id.company_number)
+          ent_sts = @repository_bs.list_matching_at_least_one_identifier([id_oc])
+          ent_sts.each do |ent_st|
+            next if (ent_st&.identifiers || []).include?(id_lei)
+
+            ent_st.identifiers << id_lei
+            @publisher.publish_many({ uid: ent_st })
+          end
+        end
+      end
+    end
+  end
+end
