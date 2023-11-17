eaa7a459d2add32a580efe1689f80d8173feeda1
Nicolas Williams <tp@tiredpixel.com>
2023-11-17 15:07:13 +0100

    [#189] extend Migrations::Base to support buffer

--- lib/register_sources_bods/migrations/base.rb
+++ lib/register_sources_bods/migrations/base.rb
@@ -5,6 +5,19 @@ require_relative '../constants/identifiers'
 module RegisterSourcesBods
   module Migrations
     class Base
+      BUFFER_SIZE = 50
+
+      def initialize
+        @buffer = []
+      end
+
+      def migrate
+        do_migrate
+        flush_buffer
+      end
+
+      private
+
       def log_doc(doc)
         identifier = doc['_source']['identifiers']&.select do |i|
           i['schemeName'] == REGISTER_SCHEME_NAME
@@ -15,6 +28,19 @@ module RegisterSourcesBods
           (identifier ? identifier['id'] : '').ljust(30),
           doc['_source']['name']
         ].join(' ')
+      end
+
+      def append_buffer(item)
+        @buffer << item
+        flush_buffer if @buffer.size >= BUFFER_SIZE
+      end
+
+      def flush_buffer
+        return if @buffer.empty?
+
+        puts "* FLUSHING #{@buffer.size} items in buffer"
+        do_flush_buffer
+        @buffer = []
       end
     end
   end
