85e66082183e59e9fcab2525620ea871b366aa43
Nicolas Williams <tp@tiredpixel.com>
2023-11-17 15:09:57 +0100

    [#189] implement M20131117OCCase fully to amend statements

--- lib/register_sources_bods/migrations/m_20131117_oc_case.rb
+++ lib/register_sources_bods/migrations/m_20131117_oc_case.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
 require_relative '../constants/identifiers'
+require_relative '../mappers/resolver_mappings'
 require_relative '../repositories/bods_statement_repository'
+require_relative '../services/publisher'
 require_relative 'base'
 
 module RegisterSourcesBods
   module Migrations
     class M20131117OCCase < Base
+      include RegisterSourcesBods::Mappers::ResolverMappings
+
       def initialize(identifiers_id_prefix = nil)
         super()
         @identifiers_id_prefix = identifiers_id_prefix || 'https://opencorporates.com/companies/gb/Sc'
         @repo = Repositories::BodsStatementRepository.new
+        @publisher = Services::Publisher.new
       end
 
-      def migrate
+      private
+
+      def do_migrate
         q_filter = {
           nested: {
             path: 'identifiers',
@@ -29,7 +36,29 @@ module RegisterSourcesBods
         }
         @repo.each(q_filter:) do |doc|
           log_doc(doc)
+          process_doc(doc)
         end
+      end
+
+      def process_doc(doc)
+        stmt = BodsStatement[doc['_source'].compact]
+        identifiers2 = stmt.identifiers.map do |i|
+          if i.schemeName == OPEN_CORPORATES_SCHEME_NAME
+            id2 = i.id.split('/')
+            identifier_open_corporates_from_company(id2[-2], id2[-1])
+          else
+            i
+          end
+        end
+        return if identifiers2 == stmt.identifiers
+
+        stmt2 = stmt.new(identifiers: identifiers2)
+        append_buffer(stmt2)
+      end
+
+      def do_flush_buffer
+        stmts_h = @buffer.to_h { |s| [s.statementID, s] }
+        @publisher.publish_many(stmts_h)
       end
     end
   end
