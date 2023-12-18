dbb37abd67d2bd13ca3eda2a943eefc1a3caad05
Nicolas Williams <tp@tiredpixel.com>
2023-12-18 22:01:45 +0100

    [#222] fix M20131213AltNames process_stmt alternateNames no method
    *sigh*

--- lib/register_sources_bods/migrations/m_20231213_alt_names.rb
+++ lib/register_sources_bods/migrations/m_20231213_alt_names.rb
@@ -49,7 +49,7 @@ module RegisterSourcesBods
 
       def process_stmt(doc, id, stmt)
         alternate_name = doc['_source']['name']
-        stmt.alternateNames ||= []
+        stmt = stmt.new(alternateNames: []) unless stmt.alternateNames
         return if stmt.alternateNames.include?(alternate_name)
 
         stmt.alternateNames << alternate_name
