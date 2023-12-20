423609449ad58675beff5ea0f62a72e93b3b3960
Nicolas Williams <tp@tiredpixel.com>
2023-12-20 18:11:53 +0100

    [#171] extend RecordDeserializer fix_parsed! to strip incorrectish identifiers
    AM data contains `identifiers` which contain neither `id` nor `uri`,
    yet `scheme`. This is technically valid BODS 0.2, since it doesn't
    required that *any* fields are present for `Identifier` (not even one!).
    But these identifiers are incorrectish, because without `id` or `uri`,
    they don't say anything useful. Much more importantly, however, this
    leads to Sources BODS merging unrelated statements together, seemingly
    because they contain the 'same' identifier.
    
    Reject all identifiers which contain neither `id` nor `uri`. This is
    unlikely to affect PSC, DK, SK data much, but it makes a difference to
    AM data.
    
    Looking ahead, it's my hope that this will be resolved in BODS 0.4.

--- lib/register_sources_bods/record_deserializer.rb
+++ lib/register_sources_bods/record_deserializer.rb
@@ -7,12 +7,17 @@ require_relative 'structs/bods_statement'
 
 module RegisterSourcesBods
   class RecordDeserializer
+    # rubocop:disable Metrics/CyclomaticComplexity
     def self.fix_parsed!(record)
       record['interests']&.each do |interest|
         v = interest.dig('share', 'exact')
         v.tr!(',', '.') if v.is_a?(String) && v.include?(',')
       end
+      record['identifiers']&.reject! do |identifier|
+        identifier['id'].nil? && identifier['uri'].nil?
+      end
     end
+    # rubocop:enable Metrics/CyclomaticComplexity
 
     def deserialize(record)
       parsed = JSON.parse(record)
