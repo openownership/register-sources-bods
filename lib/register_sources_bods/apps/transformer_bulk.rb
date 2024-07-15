3e6501e15453dc1841d623f5d08b23bd8d362e65
Nicolas Williams <tp@tiredpixel.com>
2024-07-15 11:50:49 +0000

    rework TransformerBulk logging to be similar to TransformerStream

--- lib/register_sources_bods/apps/transformer_bulk.rb
+++ lib/register_sources_bods/apps/transformer_bulk.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'logger'
 require 'redis'
 require 'register_common/services/file_reader'
 require 'register_common/utils/expiring_set'
@@ -16,6 +17,7 @@ module RegisterSourcesBods
 
       def initialize(namespace:, namespace_transformed:, parallel_files:, record_processor:, record_struct:,
                      s3_adapter:)
+        @logger = Logger.new($stdout)
         @redis = Redis.new(url: ENV.fetch('REDIS_URL'))
         @s3_bucket = ENV.fetch('BODS_S3_BUCKET_NAME')
         @file_reader = RegisterCommon::Services::FileReader.new(
@@ -31,10 +33,12 @@ module RegisterSourcesBods
         @exp_set = RegisterCommon::Utils::ExpiringSet.new(
           redis: @redis, namespace: namespace_transformed, ttl: REDIS_TRANSFORMED_TTL
         )
+        @files_n = Hash.new(0)
       end
 
       def transform(s3_prefix)
         s3_paths = @s3_adapter.list_objects(s3_bucket: @s3_bucket, s3_prefix:)
+        @logger.debug "PARALLEL: #{@parallel_files}"
         s3_paths.each_slice(@parallel_files) do |s3_paths_batch|
           threads = []
           s3_paths_batch.each do |s3_path|
@@ -48,20 +52,22 @@ module RegisterSourcesBods
 
       def process_s3_path(s3_path)
         if file_processed?(s3_path)
-          print "Skipping #{s3_path}\n"
+          @logger.debug "[#{s3_path}] SKIPPING"
           return
         end
-        print "#{Time.now} Processing #{s3_path}\n"
+        @logger.debug "[#{s3_path}] PROCESSING"
         @file_reader.read_from_s3(s3_bucket: @s3_bucket, s3_path:) do |rows|
-          process_rows rows
+          process_rows(rows, s3_path)
         end
         mark_file_complete(s3_path)
-        print "#{Time.now} Completed #{s3_path}\n"
+        @logger.debug "[#{s3_path}] PROCESSED"
       end
 
-      def process_rows(rows)
+      def process_rows(rows, s3_path)
         rows.each do |record_data|
+          @files_n[s3_path] += 1
           record_h = JSON.parse(record_data, symbolize_names: true)
+          @logger.info "[#{s3_path}] [#{format('%9s', @files_n[s3_path])}] #{record_h[:data][:links][:self]}"
           etag = record_h.dig(:data, :etag)
           next if etag && @exp_set.sismember(REDIS_TRANSFORMED_KEY, etag)
 
