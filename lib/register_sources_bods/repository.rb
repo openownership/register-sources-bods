f08613ece8090b5600458ce3672e43815a128672
Nicolas Williams <tp@tiredpixel.com>
2024-07-16 19:25:03 +0000

    Revert "optimise Repository store to force refresh"
    Whilst this showed promise in benchmarks, I'm not convinced that the
    effect when run in production is beneficial. In fact, it seems to make
    processing in parallel even worse, likely because the Elasticsearch
    index refreshes are then not aligned.
    
    This reverts commit 0fc866acbf2d68292ee05d199d4b21f32c068cf1.

--- lib/register_sources_bods/repository.rb
+++ lib/register_sources_bods/repository.rb
@@ -308,7 +308,7 @@ module RegisterSourcesBods
         }
       end
 
-      refresh = await_refresh || @await_refresh ? true : false
+      refresh = await_refresh || @await_refresh ? :wait_for : false
 
       result = client.bulk(body: operations, refresh:)
 
